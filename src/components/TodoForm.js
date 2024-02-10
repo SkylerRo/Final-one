e9021b7e1d9c1d846140f36183b36b6e0db67b41
SkylerRo <149194025+SkylerRo@users.noreply.github.com>
2024-02-09 19:19:40 -0500

    part-three

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,35 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const handleChange = e => {
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
 
-const handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
     props.onSubmit({
-        id: Math.floor(Math.random() * 10000),
-        text: input
+      id: Math.floor(Math.random() * 10000),
+      text: input
     });
-
-    setInput('')
-
-}
+    setInput('');
+  };
 
   return (
-    <form className="todo-form" onSubmit= {handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add todo"
-        value={input}
-        name="text"
-        className="todo-input"
-        onChange={handleChange}
-      />
-      <button className="todo-button">Add todo</button>
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Add a todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Add todo
+          </button>
+        </>
+      )}
     </form>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,39 +1,57 @@
-import React, { useState } from 'react'
-import TodoForm from './TodoForm'
-import Todo from './Todo'
-
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-const addTodo = todo => {
-    if(!todo.text || /^\s*$/.test(todo.text)) {
-        return
+  const addTodo = todo => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
 
-    const newTodos = [todo, ...todos]
+    const newTodos = [todo, ...todos];
 
-    setTodos(newTodos)
-}
+    setTodos(newTodos);
+    console.log(...todos);
+  };
 
-const completeTodo = id => {
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
-        if (todo.id === id) {
-            todo.isComplete = !todo.isComplete
-        }
-        return todo
-    })
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
     setTodos(updatedTodos);
-}
-
+  };
 
   return (
-    <div>
-        <h1>Whats the plan for today</h1>
-        <TodoForm onSubmit={addTodo} />
-        <Todo />
-    </div>
-  )
+    <>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
 }
 
-export default TodoList
+export default TodoList;
