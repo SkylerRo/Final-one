1088a7b909bf274a25245e5e2060166e3bab5d78
SkylerRo <149194025+SkylerRo@users.noreply.github.com>
2024-02-13 13:11:35 -0500

    part15

---
+++ pages/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function MainPage() {
+  return (
+    <main> 
+        <h1>MainPage</h1>
+
+        <div>
+         <form>
+            <h3>Sign In</h3>
+            <label>Email</label>
+            <input type="text" placeholder="Email or Phone"/>
+         </form>
+
+        </div>
+        </main>
+  )
+}
+
+export default MainPage
